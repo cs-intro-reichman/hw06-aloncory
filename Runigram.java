03e36d08ec18423a9d076ba3c59f6968c8cd69df
Alon Cory <alon.cory@runi.ac.il>
2024-01-22 19:31:20 +0200

    Update Runigram.java

--- Runigram.java
+++ Runigram.java
@@ -144,11 +144,11 @@ public class Runigram {
 	 */
 	public static Color[][] scaled(Color[][] image, int width, int height) {
 		Color[][] scaledImage = new Color[width][height];
-		int w0 = image[0].length; // Gets the width of the original image
-		int h0 = image.length; // Gets the height of the original image
+		int w0 = image.length; // Gets the width of the original image
+		int h0 = image[0].length; // Gets the height of the original image
 		double widthScaleFactor = (double) w0 / width; 
+		double heightScaleFactor = (double) h0 / height;
 		for (int i = 1; i <= width; i++) {
-			double heightScaleFactor = (double) h0 / height;
 			for (int j = 1; j <= height; j++) {
 				scaledImage[i - 1][j - 1] = image[(int)((i - 1) * widthScaleFactor)][(int)((j - 1) * heightScaleFactor)];
 			}
